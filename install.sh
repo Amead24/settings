5f208a5e8d7f5c980445bca7a7f1704763b77ffc
Anthony Mead <9004841+Amead24@users.noreply.github.com>
2018-02-16 14:46:06 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,4 +9,14 @@ git clone https://github.com/Lokaltog/vim-distinguished.git .vim/colors/
 cp .vim/colors/distinguished/colors/distinguished.vim .vim/colors/distinguished.vim
 
 # plugin - surround
-git clone https://github.com/tpope/vim-surround.git .vim/bundle/surround
+# git clone https://github.com/tpope/vim-surround.git .vim/bundle/surround
+
+# complete setup - awesome vim
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+
+# update my preferences
+rm .vim_runtime/my_configs.vim
+cp config/settings.vim .vim_runtime/my_configs.vim
+
+# install everything
+sh ~/.vim_runtime/install_awesome_vimrc.sh
