fc5df0c941948ab4d041222b71532b336ec2c01e
Anthony Mead <9004841+Amead24@users.noreply.github.com>
2018-02-16 14:33:57 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
 cd ~
-mkdir .vim .vim/colors
-cd .vim/colors
+mkdir .vim
+
+# colorscheme - distinguished 
+git clone https://github.com/Lokaltog/vim-distinguished.git .vim/colors/
+cp .vim/colors/distinguished/colors/distinguished.vim .vim/colors/distinguished.vim
+
+# plugin - surround
+git clone https://github.com/tpope/vim-surround.git .vim/bundle/surround
+
+source ~/
