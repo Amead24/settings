6165a6b5b078235938111a49421a4b2205128d6f
Anthony Mead <9004841+Amead24@users.noreply.github.com>
2018-03-20 13:53:35 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-set -e
-
 cd ~
 rm -rf .vim*
 mkdir .vim
 
 # colorscheme - distinguished 
-git clone https://github.com/Lokaltog/vim-distinguished.git .vim/colors/
-cp .vim/colors/colors/distinguished.vim .vim/colors/distinguished.vim
-rm -rf .vim/colors/colors
+git clone https://github.com/Lokaltog/vim-distinguished.git distinguished
+cp --parents distinguished/colors/distinguished.vim .vim/colors/distinguished.vim
+rm -rf distinguished
 
 # complete setup - awesome vim
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 
-# update my preferences
+# update my preferences - must git clone as `setup`
 cp ~/setup/config/settings.vim .vim_runtime/my_configs.vim
 
 # install everything
