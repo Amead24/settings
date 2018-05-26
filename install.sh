45a7347d672bb8d5d14947c64a55efaeb1a2a25f
Anthony Mead <9004841+Amead24@users.noreply.github.com>
2018-05-25 23:17:32 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,8 +11,11 @@ rm -rf distinguished
 # complete setup - awesome vim
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 
-# update my preferences - must git clone as `setup`
-cp setup/my_configs.vim .vim_runtime/my_configs.vim
+# update my preferences
+cp settings/my_configs.vim .vim_runtime/my_configs.vim
 
 # install everything
 sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+# copy over tmux configuration
+cp settings/.tmux.conf ~/.tmux.conf
