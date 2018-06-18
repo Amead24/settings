7d0df41983e2f1beb384b59ebb6b2d6d12dc4f72
Anthony Mead <9004841+Amead24@users.noreply.github.com>
2018-06-18 15:01:38 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,17 +5,17 @@ mkdir .vim
 
 # colorscheme - distinguished 
 git clone https://github.com/Lokaltog/vim-distinguished.git distinguished
-cp --parents distinguished/colors/distinguished.vim .vim/colors/distinguished.vim
+cp distinguished/colors/distinguished.vim .vim/colors/distinguished.vim
 rm -rf distinguished
 
 # complete setup - awesome vim
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 
 # update my preferences
-cp settings/my_configs.vim .vim_runtime/my_configs.vim
+cp ./my_configs.vim ./.vim_runtime/my_configs.vim
 
 # install everything
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
 # copy over tmux configuration
-cp settings/.tmux.conf ~/.tmux.conf
+cp ./.tmux.conf ~/.tmux.conf
