10bd952a8291eefffa0cdb5fe302311f3ae31dae
Ubuntu <ubuntu@ip-10-0-1-217.ec2.internal>
2018-08-13 17:26:54 +0000

    test

--- install.sh
+++ install.sh
@@ -2,21 +2,20 @@
 rm -rf ~/.vim*
 mkdir ~/.vim
 
-# colorscheme - distinguished
-git clone https://github.com/Lokaltog/vim-distinguished.git ~/distinguished
-cp --parents ~/distinguished/colors/distinguished.vim ~/.vim/colors/distinguished.vim
-rm -rf ~/distinguished/
+# # complete setup - awesome vim
+# git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+# # install everything
+# sh ~/.vim_runtime/install_awesome_vimrc.sh
 
-# complete setup - awesome vim
-git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+# colorscheme - distinguished
+cp ./colors/ ~/.vim/colors/
 
 # update my preferences
-cp ./vim.conf ~/.vim_runtime/my_configs.vim
-
-# install everything
-sh ~/.vim_runtime/install_awesome_vimrc.sh
+cp ./vim.conf ~/.vimrc
+source ~/.vimrc
 
 # copy over tmux configuration
 cp ./tmux.conf ~/.tmux.conf
+source ~/.tmux.conf
 
 cd ~
